49d3a21df1a61b50e4776658e73ae19a5bd56cb2
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-05-31 19:44:14 +0300

    Add user creation endpoint to `createServer` function

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,35 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
 
 function createServer() {
   // Use express to create a server
   // Add a routes to the server
   // Return the server (express app)
+  const app = express();
+  const users = [];
+  let nextId = 1;
+
+  app.use(express.json());
+
+  app.post('/users', (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required' });
+    }
+
+    const newUser = {
+      id: nextId++,
+      name,
+    };
+
+    users.push(newUser);
+
+    return res.status(201).json(newUser);
+  });
+
+  return app;
 }
 
 module.exports = {
